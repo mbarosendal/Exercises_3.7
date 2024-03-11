a814a00eeb5dbdfc4b30892ccfc8a615eb4a07d5
Michael Rosendal <65503882+mbarosendal@users.noreply.github.com>
2024-03-11 20:21:41 +0100

    Exercise 3.2 in progress - adding items to menu and printing it are working

---
+++ Exercise 5/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    internal class Menu
+    {
+        public string Title { get; set; }
+        private int itemCount = 0;
+        private MenuItems[] menuItemsArray;
+
+        // The constructor
+        public Menu(int itemCount, string menuTitle)
+        {
+            this.itemCount = itemCount;
+            Title = menuTitle;
+            menuItemsArray = new MenuItems[this.itemCount];
+        }
+
+        public void SetItems()
+        {
+            for (int i = 0; i < itemCount; i++)
+            {
+                Console.WriteLine(i <= 0 ? "Type name of menu item: " : "Type name of next menu item: ");
+                string menuItemName = Console.ReadLine();
+                menuItemsArray[i] = new MenuItems();
+                this.menuItemsArray[i].Title = menuItemName;
+            }
+        }
+
+        public void Show()
+        {
+            Console.Clear();
+            
+            Console.WriteLine(this.Title + "\n");
+            for (int i = 0; i < menuItemsArray.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} " + this.menuItemsArray[i].Title);
+            }
+
+            Console.WriteLine("\n(Tryk menupunkt eller 0 for at afslutte)");
+        }
+    }
+}
--- Exercise 5/Program.cs
+++ Exercise 5/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exercise_5;
+
+Console.WriteLine("What is the name of the menu?");
+string menuTitle = Console.ReadLine();
+Console.WriteLine("How menu items are on the menu?");
+int.TryParse(Console.ReadLine(), out var numMenuItems);
+
+Menu newMenu = new Menu(numMenuItems, menuTitle);
+
+newMenu.SetItems();
+newMenu.Show();
