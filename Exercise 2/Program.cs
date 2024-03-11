4962866ed5b80d4330f73f6f6f59830f937f42cb
Michael Rosendal <65503882+mbarosendal@users.noreply.github.com>
2024-03-11 13:41:04 +0100

    Finished exercise 1.3

--- Exercise 2/Program.cs
+++ Exercise 2/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int[] ageArray = new int[4] { 27, 28, 33, 35 };
+
+Console.Write("Type age to look for: ");
+int findAge = int.Parse(Console.ReadLine());
+
+void FindAge()
+{
+    for (int i = 0; i < ageArray.Length; i++)
+    {
+        if (ageArray[i] == findAge)
+        {
+            Console.WriteLine($"Index {i}: Age {findAge} was found here." + "\n <Ending Search.>");
+            break;
+        }
+    }
+
+}
+
+FindAge();
+
+// Alternative solution with bool
+
+//bool ageFound;
+//int foundAtIndex = 0;
+
+//void FindAge()
+//{
+//    for (int i = 0; i < ageArray.Length; i++)
+//    {
+//        if (ageArray[i] == findAge)
+//        {
+//            ageFound = true;
+//            foundAtIndex = i;
+//            break;
+//        }
+//    }
+//}
+
+//FindAge();
+
+//if (ageFound = true)
+//{
+//    Console.WriteLine("Age was found in the array at index " + foundAtIndex);
+//}
+
