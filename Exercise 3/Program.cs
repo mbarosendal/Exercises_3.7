e3b029667b7402c3674051b9cf2205b3bed6e6d4
Michael Rosendal <65503882+mbarosendal@users.noreply.github.com>
2024-03-11 14:06:54 +0100

    Finished exercise 1.4

--- Exercise 3/Program.cs
+++ Exercise 3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.Write("How many people are there? ");
+int numOfPeople = int.Parse(Console.ReadLine());
+
+int[] ageOfPeople = new int[numOfPeople];
+string[] namesOfPeople = new string[numOfPeople];
+string[] ordinals = new string[3] {"1st", "2nd", "3rd" };
+
+// Fill two arrays with values corresponding to the name and age of a person, so that e.g. index 0 in both relate to the same person
+for (int i = 0; i < numOfPeople; i++)
+{
+    Console.WriteLine("What is the name of the {0} person?", i <= 3 ? ordinals[i]: i + "th");
+    string name = Console.ReadLine();
+    namesOfPeople[i] = name ?? "<Not Specified>";
+
+    for (int j = 0; j < numOfPeople; j++)
+    {
+        Console.WriteLine($"How old is {namesOfPeople[i]}?");
+        int age = int.Parse(Console.ReadLine());
+        ageOfPeople[i] = age;
+        break;
+    }
+}
+
+for (int i = 0; i < numOfPeople; i++)
+    Console.WriteLine(namesOfPeople[i] + " is " + ageOfPeople[i] + "years old.");
+
+
+
