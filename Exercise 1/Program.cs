7b0f748bd0d1f842ef2bdddf8a24fedfa11f7d6b
Michael Rosendal <65503882+mbarosendal@users.noreply.github.com>
2024-03-11 13:18:42 +0100

    Add project files.

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,8 @@
+﻿int ageA = 28;
+int ageB = 27;
+int ageC = 33;
+int ageD = 35;
+int sumOfAges = ageA + ageB + ageC + ageD;
+
+Console.WriteLine(ageA + "\n" + ageB + "\n" + ageC + "\n" + ageD);
+Console.WriteLine("Average age is: " + (sumOfAges / 4));
---
+++ Exercise 10/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] ageArray = new int[4];
+ageArray[0] = 27;
+ageArray[1] = 28;
+ageArray[2] = 33;
+ageArray[3] = 35;
+int sumOfAges = 0;
+
+// Print the ages of the array
+foreach (int age in ageArray)
+    Console.WriteLine(age);
+
+// Find sum of the ages of the array
+for (int i = 0; i < ageArray.Length; i++)
+{
+    sumOfAges += ageArray[i];
+}
+
+Console.WriteLine("The average age is: " + (sumOfAges / 4));
+
+// Exercise 1.2.1 - Alternative Array Declaration
+//int[] ageArray = new int[4] {27, 28, 33, 35};
